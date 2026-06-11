58e6ed94c5d791dffc8727f3a831192e01dc1b67
Rajat Kumar <rajat28012006@gmail.com>
2026-06-11 16:12:54 +0530

    fix

--- db/drizzle.ts
+++ db/drizzle.ts
@@ -1,8 +1,11 @@
 import { neon } from "@neondatabase/serverless";
 import { config } from "dotenv";
 import { drizzle } from 'drizzle-orm/neon-http';
+const dbwait = async()=>{
+    const sql = neon(process.env.DATABASE_URL!)
+    return drizzle({client : sql});
+}
 
-const sql = neon(process.env.DATABASE_URL!)
+export const db = await dbwait();
 // console.log("DATABASE_URL exists:", !!process.env.DATABASE_URL);
 // console.log("DATABASE_URL prefix:", process.env.DATABASE_URL?.slice(0, 30));
-export const db = drizzle({client : sql});
